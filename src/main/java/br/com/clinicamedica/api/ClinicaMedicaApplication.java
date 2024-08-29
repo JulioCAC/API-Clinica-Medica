836735bb462958141f88c499c57d8006cfd14b13
JulioCAC <juliocac999@gmail.com>
2024-08-28 21:36:22 -0300

    first commit

---
+++ src/main/java/br/com/clinicamedica/api/ClinicaMedicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinicamedica.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinicamedica/api/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicamedica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
